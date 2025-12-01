60c16b259f579aa905e31b00d9943823d738c4e8
ken kagai <jibken80@gmail.com>
2025-12-01 17:08:37 +0300

    Import home page and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
-
+import HomePage from './pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-center text-3xl text-amber-600">Hello There from Tailwind🖐</h1>
+      <HomePage />
     </>
   )
 }
