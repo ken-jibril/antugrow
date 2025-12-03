9588c2144af0dd67b4f25d0a2931bb43f108934f
ken kagai <jibken80@gmail.com>
2025-12-03 11:20:50 +0300

    Comment out an unwanted section, and add spacing for readability.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -120,7 +120,7 @@ export default function HomePage() {
               />
               <p className='text-xl sm:text-xl md:text-2xl'>Farmer using 
                 <span className="text-blue-600 hover:text-blue-700 hover:border-b-2 transition-transform duration-300">
-                  <a href="https://antugrow.com/" target="_blank" rel="noopener noreferrer">Antugrow-Farm Advisor</a>
+                  <a href="https://antugrow.com/" target="_blank" rel="noopener noreferrer"> Antugrow-Farm Advisor</a>
                   </span>
               </p>
             </div>
